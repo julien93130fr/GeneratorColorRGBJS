6baf02fa01320cd8b3fbeb4922f18a3f0abacc29
julien93130fr <120179983+julien93130fr@users.noreply.github.com>
2023-01-17 10:54:58 +0100

    Add files via upload

---
+++ generateurDeColorRgb/script.js
@@ -0,0 +1,32 @@
+// Déclaration des variables :
+let a, b, c, nouveauCode;
+
+// Récupération des éléments du DOM :
+let colorDiv = document.querySelector('.color-div');
+let colorCode = document.querySelector('.color-code');
+
+// Génération de couleur quand on presse la touche espace du clavier
+let codeRGB = document.addEventListener('keydown', function(e){
+    if(e.keyCode === 32){
+                // On génère trois valeurs aléatoires pour chacune des trois valeurs r g et b
+                a = valeurAleatoire();
+                b = valeurAleatoire();
+                c = valeurAleatoire();
+
+                  // Stockage de la valeur du code rgb :
+                  nouveauCode ='rgb('+ a + ',' + b + ',' + c + ')';
+
+                   // Mise à jour des éléments du DOM : 
+                   colorDiv.style.backgroundColor = nouveauCode;
+                   colorCode.textContent = nouveauCode;
+
+
+    }
+});
+
+function valeurAleatoire() {
+    return Math.floor(Math.random()*256)   
+}
+// Chaque valeur est comprise entre 0 et 255.
+// Math.random génère une valeur aléatoire dans l'intervalle [0, 1[, donc la valeur de 256 ne sera jamais atteinte (car 1 est exclu -> au max 255.9999)
+// On arrondi cette valeur à l'entier en dessous ( -> 255 au max)
